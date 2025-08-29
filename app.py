44b99a6c2904ff0786828729d42d9fd01b7f04cf
njl <nathan.j.langman@googlemail.com>
2025-08-29 13:31:45 +0100

    updating dashboard

--- app.py
+++ app.py
@@ -47,18 +47,14 @@ genomes = genome_df['genome'].unique()
 genomes.sort()
 snp_df = get_snps()
 mutpred_df = get_mutpred()
+genes = snp_df['snp__gene_name'].unique()
+genes.sort()
 
 
-st.title(":dna: M. Bovis SNP Viewer :dna:")
-st.subheader("View information on SNPs from M. bovis wild strains")
-view_options = st.selectbox(
-    "Dashboard options",
-    ("Select...", "Specific genome", "SNP list", "Characteristic SNPs", "Help"),
-)
-
-if view_options == 'Specific genome':
+def sample_view():
+    st.subheader("Sample viewer")
     genome_input = st.selectbox(
-        "Choose a genome",
+        "Choose a sample",
         genomes
     )
     spec_genome_df = genome_df[genome_df['genome']==genome_input]
@@ -66,7 +62,7 @@ if view_options == 'Specific genome':
     host = spec_genome_df['Host'].values[0]
     classification = spec_genome_df['classification'].values[0]
 
-    st.write(f'Genome: {genome_input}')
+    st.write(f'Sample: {genome_input}')
     st.write(f'Host: {host}')
     st.write(f'Sublineage: {classification}')
     st.write(f"SNP count: {len(spec_genome_df)}")
@@ -77,21 +73,66 @@ if view_options == 'Specific genome':
     st.subheader("Missense genes")
     st.dataframe(spec_genome_df[spec_genome_df['snp__effect']=='missense_variant']['snp__gene_name'].value_counts().rename('Missense SNP count').reset_index())
 
-elif view_options == 'SNP list':
+
+def gene_view():
+    st.subheader("Gene viewer")
+    gene_input = st.selectbox(
+        "Choose a gene",
+        genes
+    )
+    spec_gene_df = genome_df[genome_df['snp__gene_name']==gene_input]
+    gene_df = snp_df[snp_df['snp__gene_name']==gene_input]
+
+    st.write(f"Total SNP count: {len(spec_gene_df)}")
+    st.write(f"Unique SNP count: {len(gene_df)}")
+    st.write(f"Unique non-synonymous SNP count: {len(gene_df[gene_df['snp__effect']!='synonymous_variant'])}")
+    st.write(f"Samples with SNP: {spec_gene_df['genome'].nunique()}")
+
+    st.subheader("SNPs")
+    st.dataframe(gene_df)
+
+    st.subheader("Full SNP List")
+    st.dataframe(spec_gene_df)
+
+
+def snp_view():
     st.subheader("SNP List")
     st.dataframe(snp_df)
 
-elif view_options == 'Characteristic SNPs':
+
+def characteristic_view():
     st.subheader("Characteristic SNPs")
     st.dataframe(mutpred_df)
 
-elif view_options == 'Help':
+
+def help_view():
     st.subheader("Help")
     st.write("This tool allows a user to display data on SNPs from M. bovis wild strains.")
     st.write("The options are to:")
-    st.write("  1) Select a specific genome and see all SNPs associated with it.")
+    st.write("  1) Select a specific sample and see all SNPs associated with it.")
+    st.write("  1) Select a specific gene and see all SNPs associated with it.")
     st.write("  2) View the full list of SNPs observed across the genomes analysed.")
     st.write("  3) View the MutPred2 predictions for 'characteristic SNPs'")
 
-else:
-    st.write('An option must be selected to display data.')
+
+def unknown_view():
+    st.subheader('Welcome to M. bovis SNP Viewer!')
+    st.write('Please select an option from the above drop-down to continue.')
+
+view_dict = {
+    'Specific genome':sample_view,
+    'Specific gene':gene_view,
+    'SNP list':snp_view,
+    'Characteristic SNPs':characteristic_view,
+    'Help':help_view
+}
+
+st.title(":dna: M. Bovis SNP Viewer :dna:")
+st.subheader("View information on SNPs from M. bovis wild strains")
+view_options = st.selectbox(
+    "Dashboard menu:",
+    ("Select...", "Specific genome", "Specific gene", "SNP list", "Characteristic SNPs", "Help"),
+)
+
+display_func = view_dict.get(view_options, unknown_view)
+display_func()
